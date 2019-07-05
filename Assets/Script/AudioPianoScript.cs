c2903e1bd5c007bded1f6e3d11b68e09731179d4
Yanis DANDO <yanis.dando@gmail.com>
2019-07-05 17:02:39 +0200

    finished? not really

---
+++ Assets/Script/AudioPianoScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPianoScript : MonoBehaviour
+{
+    public AudioClip pianoMusic;
+    public AudioSource audiosource;
+
+    public GameObject mycamera;
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource.clip = pianoMusic;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, mycamera.transform.position);
+        // Debug.Log("Distance: "+distance);
+        if(distance > 5.0)
+        {
+            wait();
+            audiosource.Play();
+        }
+    }
+
+    IEnumerator wait() {
+        yield return new WaitForSeconds(90);
+    }
+}
+
+
---
+++ Assets/Script/Portkitomb.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portkitomb : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    public bool enter = false;
+    public bool enter2 = false;
+    public GameObject camera = null;
+    public BoxCollider doorCollider = null; 
+
+    void Update()
+    { 
+        if (enter && transform.rotation.x < 0.45)
+        {
+            transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+        }
+        
+        if (enter2){
+            camera.transform.position= new Vector3(2.76f, 8.7f, 6.1f);
+            camera.transform.localScale = new Vector3(0.1F, 0.1F, 0.1F);
+
+            doorCollider.size = new Vector3(1.0F, 2.5F, 2.52F);
+            doorCollider.center = new Vector3(-0.01F, 0.79F, 0.76F);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && enter2 && !enter)
+        {
+            enter = true;
+        }
+
+        if (other.gameObject.tag == "Player" && !enter2 && !enter)
+        {
+            enter2 = true;
+        }
+    }
+}
